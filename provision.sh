1581a125c06cb0a7c82530eb6fb2f6a38009a832
Kyle Snavely <kjsnavely@gmail.com>
2016-01-17 19:22:24 -0500

    Notes/commands to eventually form a provisioner

---
+++ provision.sh
@@ -0,0 +1,12 @@
+ssh -i IDENTITY ubuntu@HOST
+sudo apt-get install -y ansible git python-dev
+git clone https://github.com/ksnavely/website.git
+deal with ansible hosts config?
+ - added localhost
+pinned nginx and runit versions, geez
+sudo ln -s . /srv/website/website
+sudo ln -s /home/ubuntu/website/website /srv/website/website
+set modes to 06400
+perms are off, set to 0764
+turned off some default nginx noise? sv stop, fix, sv start
+

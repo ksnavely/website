1cd3f19ace41b7319df9ec387007e457e7b91e17
Kyle Snavely <kjsnavely@gmail.com>
2016-03-07 20:45:03 -0500

    WIP user creation endpoint

--- website/server/__init__.py
+++ website/server/__init__.py
@@ -1,12 +1,24 @@
 import website
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+from werkzeug import exceptions
+
 
 def build_application():
     app = Flask(__name__)
     return app
 
+
 app = build_application()
+
+
+@app.route('/')
+def index():
+    """
+    Returns the frontpage.
+    """
+    return render_template('index.html')
+
 
 # Version endpoint
 @app.route('/version')
@@ -21,12 +33,30 @@ def version():
 
     return jsonify(info)
 
-@app.route('/')
-def index():
+
+@app.route('/account', methods=["POST"])
+def new_user():
     """
-    Returns frontpage
+    Create a new user. Expects the following JSON data:
+      - username: the desired username
+      - password: the desired password
     """
-    return render_template('index.html')
+    if not request.json:
+        raise exceptions.BadRequest("Request must include JSON content")
+
+    username = request.json.get("username")
+    password = request.json.get("password")
+
+    if None in [username, password]:
+        raise exceptions.BadRequest(
+            "Request must include JSON username and password."
+        )
+
+    ack = accounts.create_account(username, password)
+    return jsonify({"created": ack.inserted_id}), 201
 
 if __name__ == "__main__":
     app.run()
+
+
+
